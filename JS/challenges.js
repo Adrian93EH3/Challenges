dc62feb20b1ffd79cb8b75735177319e31fb3519
Adrian Diaz <adrian93eh3@gmail.com>
2021-03-08 23:06:00 -0500

    removed a comment, finished the challenge using two different results to do so

--- JS/challenges.js
+++ JS/challenges.js
@@ -1,15 +1,19 @@
 // REVERSE STRING
 function reverseString(str) {
-    //The split() method is used to split a string into an array of substrings,
-    // and returns the new array.
-    // If an empty string ("") is used as the separator, the string is split between each character.
-    var array = str.split(""); // puts line 7 into an array
-    var reverseArray = array.reverse();
-    console.log(reverseArray); // returns ["o", "l", "l", "e", "h"]
-    // The join() method returns the array as a string. If there were multiple elements,
-    // each element will be separated by a comma.
-    var reversedArray = reverseArray.join("")
-    return reversedArray;
+    // //The split() method is used to split a string into an array of substrings,
+    // // and returns the new array.
+    // // If an empty string ("") is used as the separator, the string is split between each character.
+    // var array = str.split("");
+    // var reverseArray = array.reverse();
+    // console.log(reverseArray); // returns ["o", "l", "l", "e", "h"]
+    // // The join() method returns the array as a string. If there were multiple elements,
+    // // each element will be separated by a comma.
+    // var reversedArray = reverseArray.join("")
+    // return reversedArray;
+
+    // Splits the array into a substring array, then reverses it, and finishes it by joining it and breaking the array.
+    return str.split("").reverse().join("");
+
   }
   
   console.log(reverseString("hello")); // olleh
