cc66ee34f7004637037e6a61b8c5c840b2a151bc
Adrian Diaz <adrian93eh3@gmail.com>
2021-03-08 22:41:51 -0500

    getting started with figuring this out, used W3 schools definition and put it as a comment so I may remember what this method does

--- JS/challenges.js
+++ JS/challenges.js
@@ -1,4 +1,10 @@
 // REVERSE STRING
 function reverseString(str) {
-
-}
+    //The split() method is used to split a string into an array of substrings,
+    // and returns the new array.
+    // If an empty string ("") is used as the separator, the string is split between each character.
+    var array = str.split(""); // puts line 7 into an array
+    return array;
+  }
+  
+  console.log(reverseString("hello")); // 
