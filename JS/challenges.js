3afea9e02f3116fd275ab62869fa61499b306d73
Adrian Diaz <adrian93eh3@gmail.com>
2021-03-08 22:56:17 -0500

    finished it, but I will now do a DRY'ing of the code using chain methoding in JS

--- JS/challenges.js
+++ JS/challenges.js
@@ -6,6 +6,10 @@ function reverseString(str) {
     var array = str.split(""); // puts line 7 into an array
     var reverseArray = array.reverse();
     console.log(reverseArray); // returns ["o", "l", "l", "e", "h"]
+    // The join() method returns the array as a string. If there were multiple elements,
+    // each element will be separated by a comma.
+    var reversedArray = reverseArray.join("")
+    return reversedArray;
   }
   
-  console.log(reverseString("hello")); // 
+  console.log(reverseString("hello")); // olleh
