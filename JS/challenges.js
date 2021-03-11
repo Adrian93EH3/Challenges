421cf1fab3d6fb2fdf9c18ff519bbd5227f2037a
Adrian Diaz <adrian93eh3@gmail.com>
2021-03-11 11:22:40 -0500

    added the prompt for the last code

--- JS/challenges.js
+++ JS/challenges.js
@@ -1,5 +1,8 @@
 // REVERSE STRING //
 function reverseString(str) {
+// Reverse the provided string. You may need to turn the string into an array before you can reverse it.
+// Your result must be a string.
+
     // FIRST WAY OF DOING THE CHALLENGE //
     // //The split() method is used to split a string into an array of substrings,
     // // and returns the new array.
