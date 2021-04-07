5fcf2cb57e85a952c83679cd1c91d092891ff351
Adrian Diaz <adrian93eh3@gmail.com>
2021-04-07 12:23:26 -0400

    building out the function

--- JS/challenges.js
+++ JS/challenges.js
@@ -123,3 +123,6 @@ titleCase("United states of america");
 
 // LARGEST NUMBER //
 // Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+function largestNumber(array) {
+  
+}
