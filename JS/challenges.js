dc6993592107d9a7b7607105a0dba342560e2d24
Adrian Diaz <adrian93eh3@gmail.com>
2021-05-03 11:05:18 -0400

    refactoring comments to use the multi-line comment way

--- JS/challenges.js
+++ JS/challenges.js
@@ -108,11 +108,11 @@ function titleCase(str4) {
     words[i] = words[i][0].toUpperCase() + words[i].slice(1);
   }
   // // EXAMPLES //
-  // var names = ["adrian", "emily"];
-  // console.log(names[0].toUpperCase()) // it results in ' ADRIAN '
-  // var name = "Adrian";
-  // console.log(name.slice(1)) // it results in ' drian '
-  // // END OF EXAMPLES //
+  /* var names = ["adrian", "emily"];
+   console.log(names[0].toUpperCase()) // it results in ' ADRIAN '
+   var name = "Adrian";
+   console.log(name.slice(1)) // it results in ' drian ' */
+  // END OF EXAMPLES //
 
   // Join the elements so it's not an array filled with a subset of strings, and instead is just one string.
   console.log(words.join(" "));
