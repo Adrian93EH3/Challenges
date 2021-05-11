e64e9055300f6092a23bfca3e33fe443057af8b2
Adrian Diaz <adrian93eh3@gmail.com>
2021-05-11 10:29:20 -0400

    done with this challenge

--- JS/challenges.js
+++ JS/challenges.js
@@ -247,5 +247,23 @@ function triArea(base, height) {
   return base * height / 2;
 }
 
-console.log(triArea(9, 5));
+console.log(triArea(10, 5));
+console.log("------------------");
+
+// INTEGER IN RANGE? //
+// Create a function that validates whether a number 'n' is within the bounds of 'lower' and 'upper'. Return false if 'n' is not an integer.
+function intWithinBounds(n, lower, upper) {
+	if(n === upper){
+		return false;
+	}
+	for(let i = lower; i <= upper; i++){
+		if(i === n){
+			return true;
+		}
+	}
+	return false;
+}
+
+console.log(intWithinBounds(3, 1, 9));
+console.log(intWithinBounds(4.5, 3, 8));
 console.log("------------------");
